d65d2c17c5ac34a489d03449527ee3c4979965b4
Burnie <W0410404@campus.nscc.ca>
2022-11-02 10:38:56 -0300

    eggs be blinky

--- Assets/scripts/TimelineTriggers.cs
+++ Assets/scripts/TimelineTriggers.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 
-public class TimelineTrigger : MonoBehaviour
+public class TimelineTriggers : MonoBehaviour
 {
     public PlayableDirector timeline;
 
